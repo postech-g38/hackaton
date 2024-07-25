aaf8e31cb15a72ede7864e39b813cf6a39fd9ff8
Luiz F <luiz.f_ferraresi@hotmail.com>
2024-07-25 20:04:15 -0300

    create models

--- src/adapters/database/models/appointments_model.py
+++ src/adapters/database/models/appointments_model.py
@@ -10,7 +10,7 @@ from src.adapters.database.models.base_model import EntityModel
 class AppointmentModel(EntityModel):
     __tablename__ = 'appointment'
     
-    patient_id: Mapped[int] = mapped_column(ForeignKey('patieent.id'))
+    client_id: Mapped[int] = mapped_column(ForeignKey('client.id'))
     doctor_id: Mapped[int] = mapped_column(ForeignKey('doctor.id'))
     record_id: Mapped[int] = mapped_column(ForeignKey('record.id'))
 
